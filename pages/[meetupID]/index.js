1270cea9f9a6b14059437f8d11b68e99ed522a24
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-11 13:15:12 +0530

    feat: add meetup  details page

---
+++ pages/[meetupID]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+export default function MeetupDetailsPage() {
+    return (
+        <MeetupDetails
+            id="1"
+            title="Youtube fan fest"
+            image="https://lh3.googleusercontent.com/xEGG6LDaFxBbQiwjTyQRQl2GZNATSLve0H62FDgIP6BMBUZSCVMN2feB-LnhM9y53eo7ZS4EjGcW5WhV2JMWW7MJ3bAdafJDI7FkSw=s1200"
+            description="Youtube fan fest"
+            address="Mumbai, India"
+        />
+    )
+}
