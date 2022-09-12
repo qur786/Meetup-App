0ad1f4598b7912cabaabcd668046e6ede0b1b0f6
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-13 00:08:59 +0530

    feat: add env support

--- db/connnect-mongo.js
+++ db/connnect-mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export async function connectionToMeetup() {
-    const uri = "mongodb+srv://m001-student:fFL0o1hKf7qHsfXp@sandbox.ukfkzxi.mongodb.net/meetups?retryWrites=true&w=majority";
+    const uri = process.env.mongodb;
     const client = new MongoClient(uri);
     await client.connect();
     const collection = client.db().collection("meetup");
