45368ca7c924bbdefcfb54ea1f3f9079000b7c9a
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-12 22:56:41 +0530

    feat: add code for posting data to the database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,12 +3,12 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 export default async function handler(req, res) {
     if (req.method === "POST") {
         const data = req.body;
-        const uri = "mongodb+srv:\/\/m001-student:m001-student@sandbox.ukfkzxi.mongodb.net/?retryWrites=true&w=majority";
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        const uri = "mongodb+srv://m001-student:fFL0o1hKf7qHsfXp@sandbox.ukfkzxi.mongodb.net/meetups?retryWrites=true&w=majority";
+        const client = new MongoClient(uri);
         await client.connect();
-        const collection = client.db("meet-ups").collection("meet-up");
-        collection.insertOne(data);
-        client.close();
+        const collection = client.db().collection("meetup");
+        await collection.insertOne(data);
+        await client.close();
         res.status(201).json({ message: "Meetup inserted." });
     }
 }
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,18 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetup() {
+    const router = useRouter();
     const onAddMeetup = (newMeetup) => {
-        console.log(newMeetup);
+        fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(newMeetup),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(() => {
+            router.push("/")
+        })
     }
     return (
         <NewMeetupForm onAddMeetup={onAddMeetup} />
